ba73ca8f21610b433db9627092d248f912eb096d
Miikka Huuskonen <miikka.huuskonen@gmail.com>
2017-11-16 13:19:46 +0200

    Changes to linuxsetup.sh file

--- setup/linuxsetup.sh
+++ setup/linuxsetup.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
-echo "#################################"
+echo "########## START ##########"
 echo "Hello, $(whoami)!"
 
+echo "setxkbmap fi"
 setxkbmap fi
+
+echo "Set timezone to Helsinki/Finland"
 sudo timedatectl set-timezone Europe/Helsinki
 
+echo "Package update and installation for git, tree, puppet, vagrant, virtualbox "
 sudo apt-get update
-sudo apt-get install -y git tree puppet
+sudo apt-get install -y git tree puppet vagrant virtualbox
 
+echo "Cloning git repository"
 git clone https://github.com/miikkahuuskonen/KeskitettyHallinta.git
 
+echo "Copying files to puppet config"
 cd KeskitettyHallinta/puppet/
 sudo cp -TRv /home/$(whoami)/KeskitettyHallinta/puppet/ /etc/puppet/
 cd /etc/puppet/
 
-sudo puppet apply -e 'class {"hellomiikka":}'
+echo "Applying site.pp"
+sudo puppet apply /etc/puppet/manifests/site.pp
 
-cat /tmp/hellomiikka.txt
 
-echo "#################################"
+echo "########## DONE ##########"
 echo "Everythings ready, happy hacking!"
