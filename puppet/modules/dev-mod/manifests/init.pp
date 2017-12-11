532a5c2f6f2941f30562adee7aef421115a9f17b
Miikka Huuskonen <miikka.huuskonen@gmail.com>
2017-12-11 18:58:35 +0200

    oma moduuli

---
+++ puppet/modules/dev-mod/manifests/init.pp
@@ -0,0 +1,41 @@
+class dev-mod {
+        File { owner => '0', group => '0', mode => '0644', }
+        Package { ensure => 'installed', allowcdrom => true, }
+	$userhome = '/home/miikkahuuskonen'
+
+        file { '/tmp/atom-amd64.deb':
+                ensure => present,
+                source => "puppet:///files/atom-amd64.deb"
+        }
+
+        file { '/tmp/slack-desktop-3.0.0-amd64.deb':
+                ensure => present,
+                source => "puppet:///files/slack-desktop-3.0.0-amd64.deb"
+        }
+
+        file { '/tmp/skypeforlinux-64.deb':
+                ensure => present,
+                source => "puppet:///files/skypeforlinux-64.deb"
+        }
+
+        package { 'atom':
+                provider => dpkg,
+                source => '/tmp/atom-amd64.deb'
+        }
+
+        package { 'slack-desktop':
+                provider => dpkg,
+                source => '/tmp/slack-desktop-3.0.0-amd64.deb'
+        }
+
+        package { 'skypeforlinux':
+                provider => dpkg,
+                source => '/tmp/skypeforlinux-64.deb'
+        }
+
+        file { "${userhome}/.config/autostart/skypeforlinux.desktop":
+                content => template('dev-mod/skypeforlinux.desktop'),
+        }
+
+}
+
